d2cb11cefa67b93b5bbed0925a66d7cfb24f8488
Emiaostein <cszwdy@gmail.com>
2017-09-12 11:49:15 +0800

    Update - video play and microphone record.

--- Karaoke/Karaoke/ViewController.swift
+++ Karaoke/Karaoke/ViewController.swift
@@ -11,94 +11,83 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    let urlOnline = URL(string: "http://vodmpqbwao2.vod.126.net/vodmpqbwao2/7618deedca2b415a8393bfb124c5a6bf_1504954277642_1504954363125_21266373-00001.mp4")!
+    let urlLocal = Bundle.main.url(forResource: "a", withExtension: "mp4")!
+    
     let engine = AVAudioEngine()
-    var aplayer: AVPlayer!
-    
-    let player = AVAudioPlayerNode()
-    
-    var layer: AVPlayerLayer!
+    let audioPlayer = AVAudioPlayerNode()
+    var videoPlayer: AVPlayer!
+    var videoPlayerLayer: AVPlayerLayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        setup()
-        
-//        let fileURL = URL(string: "http://vodmpqbwao2.vod.126.net/vodmpqbwao2/7618deedca2b415a8393bfb124c5a6bf_1504954277642_1504954363125_21266373-00001.mp4")!
-        
-//        player.play()
-        
-//        AVPlayer(url: )
-//        let item = AVPlayerItem(url: URL(string: "http://vodmpqbwao2.vod.126.net/vodmpqbwao2/7618deedca2b415a8393bfb124c5a6bf_1504954277642_1504954363125_21266373-00001.mp4")!)
         
         try! AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayAndRecord, with: [.interruptSpokenAudioAndMixWithOthers, .allowBluetooth, .defaultToSpeaker])
         try! AVAudioSession.sharedInstance().setActive(true)
-        
-        
-        let fileURL = Bundle.main.url(forResource: "b", withExtension: "mp4")!
-        let item = AVPlayerItem(url: fileURL)
-        aplayer = AVPlayer(playerItem: item)
-        
-        
-        
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        layer = AVPlayerLayer(player: aplayer)
-        layer.frame = UIScreen.main.bounds
-        view.layer.addSublayer(layer)
-        
-        print(layer.isReadyForDisplay)
-//        print(layer.player)
-        
-        ready()
+
+        setupMicAndAudio()
+        setupPlayerAndLayerThenStart()
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        if let layer = layer {
-            layer.frame = UIScreen.main.bounds
-        }
-        
-    }
-    
-    func ready() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { 
-            if self.layer.isReadyForDisplay {
-                self.aplayer.play()
-                do {
-                    try self.engine.start()
-                    self.player.play()
-                } catch let error {
-                    print(error)
-                }
-            } else {
-                print("not ready")
-                self.self.ready()
-            }
+        if let layer = videoPlayerLayer {
+            layer.frame = view.bounds
         }
     }
+}
+
+// AVPlayerLayer
+extension ViewController {
     
-    func setup() {
+    func setupPlayerAndLayerThenStart() {
         
-        // mainMixer
+        // video player
+        let url = urlLocal
+        let item = AVPlayerItem(url: url)
+        videoPlayer = AVPlayer(playerItem: item)
+        
+        
+        // video player layer
+        videoPlayerLayer = AVPlayerLayer(player: videoPlayer)
+        videoPlayerLayer.frame = view.bounds
+        view.layer.addSublayer(videoPlayerLayer)
+        
+        // start
+        videoPlayer.play()
+    }
+}
+
+
+// Mic
+extension ViewController {
+    
+    func setupMicAndAudio() {
         let mainMixer = engine.mainMixerNode
         
-        // mic
+        
         let mic = engine.inputNode!
         mic.volume = 1.0
         engine.connect(mic, to: mainMixer, format: mic.inputFormat(forBus: AVAudioNodeBus.allZeros))
         
-//         player
         
-        player.volume = 1.0
-        engine.attach(player)
+        // player
+//        let fileURL = urlLocal
+//        let file = try! AVAudioFile(forReading: fileURL)
+//        audioPlayer.volume = 1.0
+//        engine.attach(audioPlayer)
+//        engine.connect(audioPlayer, to: mainMixer, format: file.processingFormat)
+//        audioPlayer.scheduleFile(file, at: nil, completionHandler: nil)
         
-        let fileURL = Bundle.main.url(forResource: "a", withExtension: "mp4")!
-        let file = try! AVAudioFile(forReading: fileURL)
         
-        engine.connect(player, to: mainMixer, format: file.processingFormat)
-        player.scheduleFile(file, at: nil, completionHandler: nil)
+        // start
+        do {
+            try self.engine.start()
+//            self.audioPlayer.play()
+        } catch let error {
+            print(error)
+        }
     }
-
 }
 
